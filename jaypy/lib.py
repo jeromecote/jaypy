01e33a02393b488d6fedf4e017fa9ac7288c5ef9
jerome cote <jeromecote98@gmail.com>
2020-11-30 14:50:28 -0500

    .

--- jaypy/lib.py
+++ jaypy/lib.py
@@ -6,6 +6,8 @@
 from os.path import split
 import pandas as pd
 import datetime
+import sys
+import webbrowser
 
 pd.set_option('display.width', 200)
 
@@ -48,28 +50,7 @@ def clean_data(data):
     return data
 
 def try_me():
-    ay = 'ay'
-    way = 'way'
-    consonant = ('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','Y','V','X','Z')
-    vowel = ('A','E','I','O','U')
-    user_word = input('Enter a word to translate to Pig Latin: ')
-    # getting first letter and making sure its a string and setting it to uppercase
-    first_letter = user_word[0]
-    first_letter = str(first_letter)
-    first_letter=first_letter.upper()
-    if first_letter in consonant:
-       print(first_letter,'is a consonant')
-       length_of_word = len(user_word)
-       remove_first_letter = user_word[1:length_of_word]
-       pig_latin=remove_first_letter+first_letter+ay
-       print('The word in Pig Latin is:',pig_latin)
-     elif first_letter in vowel:
-       print(first_letter,'is a vowel')
-       pig_latin=user_word+way
-       print('The word in Pig Latin is:',pig_latin)
-     else:
-       print('I dont know what',first_letter,'is')
-
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
